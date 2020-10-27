491e9cac88b8a49cefa8048ce3da760d0163780e
chenboshuo <chenboshuo.hi@163.com>
2020-10-27 21:59:58 +0800

    :sparkles: add the draw method.

--- regex_parser/src/Automata.py
+++ regex_parser/src/Automata.py
@@ -2,7 +2,8 @@
 filename src/Automata.py
 reference https://github.com/sdht0/automata-from-regex/blob/master/AutomataTheory.py
 """
-# from collections import defaultdict
+from matplotlib import pyplot as plt
+import networkx as nx
 
 
 class Automata:
@@ -32,7 +33,7 @@ class Automata:
     def add_final_states(self, *states):
         for state in states:
                 self.final_states.add(state) 
-                
+
     def add_transition(self, from_state, to_state, input_symbols: set):
         self.states.add(from_state)
         self.states.add(to_state)
@@ -69,21 +70,51 @@ class Automata:
             f"final state:\t{self.final_states}\n" \
             f"transitions:\n{trans}" 
 
+    def draw(self,save=None):
+        """
+        draw the graph
+        @param save the save path
+        reference https://stackoverflow.com/a/20382152 
+        """
+        # G = nx.Graph()
+        G = nx.DiGraph()
+        node_labels = {}
+        for from_state, to_states in self.transitions.items():
+            for to_state, symbols in to_states.items():
+                G.add_edge(from_state,to_state,trans=symbols)
+                node_labels[from_state] = str(from_state)
+                node_labels[to_state] = str(to_state)
+        
+        pos = nx.spring_layout(G)
+        nx.draw(G,pos,node_color='#dcfc7c')
+        nx.draw_networkx_labels(G,pos,labels=node_labels)
+        edge_labels = nx.get_edge_attributes(G,'trans')
+        nx.draw_networkx_edge_labels(G, pos, labels = edge_labels)
+
+        nx.draw_networkx_nodes(G,pos,nodelist=list(self.final_states))
+        nx.draw_networkx_nodes(G,pos,nodelist=[self.start_state],node_color='#fc7c7c')
+
+
+        if save:
+            plt.savefig(save)
+
 if __name__ == "__main__":
     test = Automata('ab')
     test.set_start_state(1)
     test.add_final_states(2)
     test.add_final_states(2)
-    test.add_transition(1,2,set('a'))
+    test.add_transition(1,2,set(['a','b']))
     test.add_transition(1,3,set('b'))
     print(test.transitions)
     print(test)
+    test.draw('../docs/figures/test_automata.pdf')
 """ output
-{1: {2: {'a'}, 3: {'b'}}}
+{1: {2: {'a', 'b'}, 3: {'b'}}}
 states: {1, 2, 3}
 start state:    1
-final state:    [2]
+final state:    {2}
 transitions:
-    1->2 on 'a'
-    1->3 on 'b'
+        1->2 on 'a'
+        1->2 on 'b'
+        1->3 on 'b'
 """
